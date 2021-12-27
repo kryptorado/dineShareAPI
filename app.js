446e3c8a1742ba3441f4fb5fce8bee3e28e04604
Dora Goczi <doribea@gmail.com>
2021-12-27 13:58:21 -0500

    add eslint, prettier and gitignore

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello World");
-})
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
 
 app.listen(80);
