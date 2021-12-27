7e1b1afa6a988f4c1abf29187269263fc667b8f3
Dora Goczi <doribea@gmail.com>
2021-12-27 15:49:45 -0500

    add morgan, nodemon, and setup instructions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dineShareAPI
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm start
+```
--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
 
 app.get('/', (req, res) => {
 	res.send('Hello World');
 });
-app.listen(80);
+app.listen(80, console.log(`Server running on port 80`));
