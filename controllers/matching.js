7b5dd80fd117b872b7e401360bf7fe47cdc03c72
Dora Goczi <doribea@gmail.com>
2022-03-06 00:15:03 -0500

    matching logic updated

--- controllers/matching.js
+++ controllers/matching.js
@@ -4,7 +4,7 @@ const asyncHandler = require('../middleware/async');
 const MatchStatus = require('../utils/MatchStatus');
 
 let usersInQueue = [];
-const currentMatches = {};
+let currentMatches = {};
 
 /**
  * currentMatches example data:
@@ -45,7 +45,6 @@ exports.enterQueue = asyncHandler(async (req, res, next) => {
 		const userInfo = {
 			uId: req.body.uId,
 			interests: req.body.interests,
-			//timeEnteredSystem: Date.now()
 		};
 		usersInQueue.push(userInfo);
 		console.log('current queue ', usersInQueue);
@@ -53,152 +52,87 @@ exports.enterQueue = asyncHandler(async (req, res, next) => {
 	return res.status(201).json({});
 });
 
+async function makeMatches() {
+	const formattedList = {}; // queue data in format required by matching server
+
+	formattedList.num_interests = DEFAULT_NUM_INTERESTS;
+	usersInQueue.forEach((user) => {
+		formattedList[user.uId] = user.interests;
+	});
+	currentMatches = {};
+
+	if (usersInQueue.length > 1) {
+		let queueCopy = [...usersInQueue]; // freeze queue
+		usersInQueue = [];
+
+		try {
+			const matches = await axios.post(MATCHING_SERVER_URL, formattedList);
+			const matchedUserIds = [];
+
+			matches.data.forEach(async (match) => {
+				const channelName = uuidv4();
+				const token = await getAgoraToken(channelName);
+
+				for (var i = 0, j = 1; i < match.length; i++, j--) {
+					matchedUserIds.push(String(match[i]));
+
+					const otherUser = String(match[j]);
+					const matchInfo = {
+						channelName,
+						token,
+						otherUser,
+					};
+					currentMatches[match[i]] = matchInfo;
+				}
+			});
+
+			// add lonely uses back in queue
+			usersInQueue = usersInQueue.concat(queueCopy);
+		} catch (error) {
+			console.log(error);
+		}
+	}
+}
+
+setInterval(makeMatches, 5000);
+
 exports.pollQueue = asyncHandler(async (req, res, next) => {
 	const userId = req.params.uId;
-	console.log(userId + ' polling queue');
-
-	// First check if the user is already matched with anyone
-	// This could occur if one of the users called pollqueue earlier
-	// If yes, (the corresponding match is present) return the match info
+	// console.log(userId + ' polling queue');
+	// const numUsersToMatch = usersInQueue.length;
 
 	if (currentMatches[userId]) {
-		res.status(200).json({
+		return res.status(200).json({
 			...currentMatches[userId],
 			...MatchStatus.FoundMatch,
 		});
+	}
+
+	if (usersInQueue.length <= 1) {
+		res.status(200).json({
+			...MatchStatus.NotEnoughUsers,
+		});
 	} else {
-		const numUsersToMatch = usersInQueue.length;
-
-		if (numUsersToMatch <= 1) {
-			res.status(200).json(MatchStatus.NotEnoughUsers);
-		} else if (numUsersToMatch === 2) {
-			const usersToMatch = [...usersInQueue]; // save user queue state because it could change anytime
-			const channelName = uuidv4(); // create channel name
-			const token = await getAgoraToken(channelName); // get Agora token
-
-			// delete matched users from queue
-			usersInQueue = usersInQueue.filter(
-				(user) => usersToMatch.indexOf(user) === -1
-			);
-
-			// add both users to the match list
-			for (var i = 0, j = 1; i < usersToMatch.length; i++, j--) {
-				const otherUser = usersToMatch[j].uId;
-
-				const matchInfo = {
-					channelName,
-					token,
-					otherUser,
-				};
-				currentMatches[usersToMatch[i].uId] = matchInfo;
-			}
-			console.log('current matches (case: 2 users): ', currentMatches);
-
-			return res.status(200).json({
-				...currentMatches[userId],
-				...MatchStatus.FoundMatch,
-			});
-		} else if (numUsersToMatch >= 3) {
-			const formattedList = {}; // queue data in format required by matching server
-
-			formattedList.num_interests = DEFAULT_NUM_INTERESTS;
-			usersInQueue.forEach((user) => {
-				formattedList[user.uId] = user.interests;
-			});
-
-			try {
-				const matches = await axios.post(MATCHING_SERVER_URL, formattedList);
-				console.log('matches: ', matches.data);
-				const matchedUserIds = [];
-
-				matches.data.forEach(async (match) => {
-					const channelName = uuidv4();
-					const token = await getAgoraToken(channelName);
-
-					for (var i = 0, j = 1; i < match.length; i++, j--) {
-						matchedUserIds.push(String(match[i]));
-
-						const otherUser = String(match[j]);
-						const matchInfo = {
-							channelName,
-							token,
-							otherUser,
-						};
-						currentMatches[match[i]] = matchInfo;
-					}
-					console.log(
-						'current matches (case: more than 3 users): ',
-						currentMatches
-					);
-
-					// remove matched user from queue
-					usersInQueue = usersInQueue.filter(
-						(user) => matchedUserIds.indexOf(user.uId) === -1
-					);
-
-					// return matched or matching states to users
-					if (currentMatches[userId]) {
-						res.status(200).json({
-							...currentMatches[userId],
-							...MatchStatus.FoundMatch,
-						});
-					} else {
-						return res.status(200).json({
-							...MatchStatus.Matching,
-						});
-					}
-				});
-			} catch (error) {
-				console.log(error);
-				return res.status(400).json({
-					...MatchStatus.Error,
-				});
-			}
-		} else {
-			console.log('something went wrong');
-			return res.status(500).json({
-				...MatchStatus.Error,
-			});
-		}
+		res.status(200).json({
+			...MatchStatus.Matching,
+		});
 	}
 });
 
 exports.doneCall = asyncHandler(async (req, res, next) => {
 	// TODO: remove requesting users from the queue as well in case they haven't already been removed
-	try {
-		delete currentMatches[req.params.uId];
-		console.log('Exit queue called by user ', req.params.uId);
-		console.log('current matches: ', currentMatches);
-
-		return res.status(204).json();
-	} catch (error) {
-		console.log(error);
-		return res.status(400).json({
-			...MatchStatus.Error,
-		});
-	}
+	console.log('Done call called by android');
+	return res.status(204).json();
 });
 
 // called when something unexpected happens and the user needs to get removed
 // from the matching process
 exports.cleanup = asyncHandler(async (req, res, next) => {
-	try {
-		// remove user from queue
-		usersInQueue = usersInQueue.filter(
-			// eslint-disable-next-line array-callback-return
-			(user) => user.uId !== req.params.uId
-		);
-		console.log('cleanup called. current queue after delete: ', usersInQueue);
-
-		delete currentMatches[req.params.uId];
-		console.log(
-			'cleanup called. current matches after delete: ',
-			currentMatches
-		);
-
-		return res.status(200).json();
-	} catch (err) {
-		console.log(err);
-		return res.status(500).json();
-	}
+	// delete matched users from queue
+	// remove user from queue
+	usersInQueue = usersInQueue.filter(
+		// eslint-disable-next-line array-callback-return
+		(user) => user.uId !== req.params.uId
+	);
+	res.status(200).json();
 });
